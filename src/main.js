89c92afa965dc0aee10df4684c7cdefdea4aa9a5
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-14 17:31:50 -0300

    End of async await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-alert('Testando');
+const myPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('OK');
+    }, 2000);
+  });
+
+// Normal way
+// myPromise()
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+async function executePromise() {
+  console.log(await myPromise());
+  console.log(await myPromise());
+  console.log(await myPromise());
+}
+
+executePromise();
+
